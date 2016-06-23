25f3dd507caf4eabf63388cbaeabf2b631884420
AndrewYongZhenNing <ayzn95@gmail.com>
2016-06-23 20:11:53 +0100

    Update ComparisonPlot.py

--- ComparisonPlot.py
+++ ComparisonPlot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# this is a github edit
+# this is a github edit again
 
 def Comparison_Function(filename,vertical_shift,period): #filename needs to be in strings
     plot = np.loadtxt(filename + ".dat")
