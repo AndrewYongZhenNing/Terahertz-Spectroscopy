421bf2a4c199f405b197f3aee17dbe30f42fecf6
AndrewYongZhenNing <ayzn95@gmail.com>
2016-07-13 14:54:26 +0100

    Compare function works now
    now to fix noise_reduction

--- PeriodFix.py
+++ PeriodFix.py
@@ -1,13 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def Compare(filename,vertical_shift): #filename needs to be in strings
+def Compare(filename): #filename needs to be in strings
     """Given a data file, this function plots a graph"""
 
     plot = np.loadtxt(filename + ".dat")
     frequency = plot[:,1]
-    photocurrent = plot[:,2] + vertical_shift
+    photocurrent = plot[:,2]
     maximum_point = np.amax(plot[1:,0])
+
+    #
+    for value in photocurrent:
+        SUM = 0
+        SUM += value
+        # average = average*1.0/len(photocurrent)
+    print 'sum', SUM
+
+    if SUM > 0:
+        photocurrent = photocurrent + SUM
+    elif SUM < 0:
+        photocurrent = photocurrent - SUM
 
     photocurrent_list = []
     frequency_list = []
@@ -36,15 +48,15 @@ def Compare(filename,vertical_shift): #filename needs to be in strings
             index_list.append(index)
         index +=1
 
-    print 'length of index is', len(index_list)
+    # print 'length of index is', len(index_list)
 
     # while j < len(index_list)-1:
     #     acquire_period = index_list[j+1] - index_list[j]
     #     period_list.append(acquire_period)
     #     j += 1
     # print 'length of period list', len(period_list)
-    print 'example index', index_list[0:15]
-    print 'example period', period_list[0:15]
+    # print 'example index', index_list[0:15]
+    # print 'example period', period_list[0:15]
 
     while k < len(index_list)-1:
         start = index_list[k]
@@ -58,8 +70,8 @@ def Compare(filename,vertical_shift): #filename needs to be in strings
 
         k += 1
 
-    print 'new frequency', len(envelope_frequency_list)
-    print 'envelope', len(envelope_photocurrent_list)
+    # print 'new frequency', len(envelope_frequency_list)
+    # print 'envelope', len(envelope_photocurrent_list)
 
     # while start < maximum_point-period and end < maximum_point:
 
@@ -79,13 +91,52 @@ def Compare(filename,vertical_shift): #filename needs to be in strings
         log_envelope_photocurrent_list.append(new_value)
     return (envelope_frequency_list,log_envelope_photocurrent_list,frequency_list,photocurrent_list)
 #
+def Noise_Reduction(data_set, nearest_neighbours):
+    """Improves the quality of the data by reducing the noise of a graph. Takes the average from the nearest neighbours of each data point."""
+    noisy_data = data_set
+    improved_data = []
+    start_index = nearest_neighbours
+
+
+    for index in range(0,nearest_neighbours): #fill in improved_data list with the first 'unused' points first (from 0th data point to (start_index-1)th data point)
+        improved_data.append(noisy_data[index])
+
+    liszt = []
+
+
+    while start_index < len(noisy_data) - nearest_neighbours:#while start_index < 11,  maximum is thus 11 in this case
+        i = -(nearest_neighbours) #left most neighbour
+        while i < nearest_neighbours+1: #maximum value of i is equal to right most value
+            liszt.append(noisy_data[(start_index)+i])
+            i +=1
+
+    # for i in range(-(nearest_neighbours),(nearest_neighbours)): #for loop is awkward, fix here
+    #     liszt.append(start_index+i)
+        # for points in noisy_data[(start_index)+i]:
+        #     liszt.append(points)
+        # print 'current list is:',liszt
+
+        total = sum(liszt)
+        average_point = (1.*total)/((nearest_neighbours*2)+1) # the 1. is there to convert fractions into accurate decimal points
+        improved_data.append(average_point)
+        liszt[:] = [] #empties the list to be re-used again
+
+        start_index += 1
+
+
+    for index in range(len(noisy_data)-nearest_neighbours,len(noisy_data)): #fill in final few points into improved data
+        improved_data.append(noisy_data[index])
+
+    return improved_data
 
 
 
-plt.figure()
-plt.grid()
+
+# plt.figure()
+plt.grid(True)
+plt.title('Graph of Envelope Photocurrent $nA$ vs Frequency $GHz$')
 plt.xlabel('Frequency $GHz$')
 plt.ylabel('Photocurrent $nA$')
-plt.plot(Compare("water",0.5)[0], Compare("water",0.5)[1], label = 'Water Run 1 Without Average', color = 'b')
-# plt.scatter(Compare("water",0.6)[2], Compare("water",0.6)[3], label = 'Water Run 1 Without Average', color = 'b')
+plt.plot(Compare("water")[0], Compare("water")[1], label = 'Water Run 1 Without Average', color = 'b')
+# plt.scatter(Compare("water")[2], Compare("water")[3], label = 'Water Run 1 Without Average', color = 'b')
 plt.show()
